719e35bb1945d7ce36556fca3cabd24c508bfc03
MarshmallowShadow <choiboseung@naver.com>
2024-09-05 10:53:31 +0900

    Feat: Added FileUtils

---
+++ file-spring-boot/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    kotlin("jvm")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //File Utils
+    implementation("commons-io:commons-io:2.7")
+    //Time-Based UUID Generator
+    implementation("com.fasterxml.uuid:java-uuid-generator:4.3.0")
+    
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
