f5b34ff393afcbbabc5c4d9a60d6de7c87218db8
MarshmallowShadow <choiboseung@naver.com>
2024-09-10 19:06:22 +0900

    Feat: Security (JWT, encryption)

---
+++ security-spring-boot/src/main/kotlin/security/jwt/JwtTokenProvider.kt
@@ -0,0 +1,56 @@
+package security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.io.DecodingException
+import io.jsonwebtoken.security.Keys
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import java.util.Date
+import javax.servlet.http.HttpServletRequest
+
+class JwtTokenProvider(
+    private val jwtSecret: String,
+) {
+    private val secretKey = Keys.hmacShaKeyFor(jwtSecret.toByteArray())
+
+    fun generateToken(username: String, role: String, expireIn: Long): String {
+        val now = Date()
+        val token = Jwts.builder()
+            .claim("username", username)
+            .claim("role", role)
+            .setIssuedAt(now)
+            .setExpiration(Date(expireIn))
+            .signWith(secretKey, SignatureAlgorithm.HS256)
+            .compact()
+
+        return token
+    }
+
+    fun resolveToken(request: HttpServletRequest): Claims? {
+        var token = request.getHeader("Authorization")
+        token = when {
+            token == null -> return null
+            token.startsWith("Bearer ") -> token.replace("Bearer ", "")
+            else -> throw DecodingException("Decoding Error: Bearer Not Found")
+        }
+        return getClaims(token)
+    }
+
+    fun getUsername(token: String) =
+        getClaims(token)["username"] as String
+
+    fun getAuthentication(claims: Claims) =
+        UsernamePasswordAuthenticationToken(claims["username"], "", getAuthority(claims))
+
+    private fun getClaims(token: String) =
+        Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .body
+
+    private fun getAuthority(claims: Claims) =
+        listOf(SimpleGrantedAuthority(claims.get("role", String::class.java)))
+}
