a80e33336e8b970b6e5ce35037ddd9fab254ac46
MarshmallowShadow <choiboseung@naver.com>
2024-09-24 16:02:10 +0900

    Fix (Security): Default CORS need to be manually enabled

--- security-config/src/main/kotlin/com/marsh/toolstash/security/CorsWebConfig.kt
+++ security-config/src/main/kotlin/com/marsh/toolstash/security/CorsWebConfig.kt
@@ -1,10 +1,16 @@
 package com.marsh.toolstash.security
 
 import org.springframework.boot.autoconfigure.AutoConfiguration
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @AutoConfiguration
+@ConditionalOnProperty(
+    prefix="com.marsh.security.cors.default",
+    name=["enabled"],
+    havingValue = "true"
+)
 class CorsWebConfig: WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
